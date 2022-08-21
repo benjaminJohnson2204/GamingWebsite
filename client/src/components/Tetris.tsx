8f37818f00a8df6e83589b7cc78056c4df1e4fc4
Benjamin Johnson <ben332004@gmail.com>
2022-08-20 23:19:47 -0700

    Re-design state system to work :)

--- client/src/components/Tetris.tsx
+++ client/src/components/Tetris.tsx
@@ -97,8 +97,11 @@ export default function Tetris(props: { user: IUser }) {
     }
     initialBoxes.push(row);
   }
-  const [boxes, setBoxes] = useState(initialBoxes);
-  const [activePiece, setActivePiece] = useState<IActivePiece>();
+
+  const [board, setBoard] = useState<{ boxes: string[][]; activePiece: IActivePiece | undefined }>({
+    boxes: initialBoxes,
+    activePiece: undefined,
+  });
 
   const initialGameSpeed = 1000; // Time in milliseconds for piece to fall 1 grid square
   const [gameSpeed, setGameSpeed] = useState(initialGameSpeed);
@@ -147,19 +150,22 @@ export default function Tetris(props: { user: IUser }) {
   }, [begun]);
 
   useEffect(() => {
-    if (activePiece && !pieceCanFall(activePiece)) {
-      placePiece(activePiece);
+    if (board.activePiece && !pieceCanFall(board)) {
+      placePiece();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [activePiece]);
+  }, [board]);
 
   const generateNewActivePiece = () => {
-    setActivePiece({
-      ...pieces[Math.floor(Math.random() * pieces.length)],
-      row: 0,
-      col: Math.floor(Math.random() * (boxesAcross - 3)),
-      clockwiseRotation: Math.floor(Math.random() * 4) / 4,
-    });
+    setBoard((prevBoard) => ({
+      boxes: prevBoard.boxes,
+      activePiece: {
+        ...pieces[Math.floor(Math.random() * pieces.length)],
+        row: 0,
+        col: Math.floor(Math.random() * (boxesAcross - 3)),
+        clockwiseRotation: Math.floor(Math.random() * 4) / 4,
+      },
+    }));
   };
 
   const getSquaresOfPiece = (piece: IActivePiece) => {
@@ -169,111 +175,146 @@ export default function Tetris(props: { user: IUser }) {
   };
 
   const hardDrop = () => {
-    setActivePiece((prevActivePiece) => {
-      let newActivePiece = prevActivePiece!;
-      while (pieceCanFall(newActivePiece!)) {
-        newActivePiece!.row += 1;
+    setBoard((prevBoard) => {
+      let newBoard = prevBoard!;
+      while (pieceCanFall(newBoard)) {
+        newBoard.activePiece!.row += 1;
       }
-      return { ...prevActivePiece!, row: newActivePiece!.row };
+      return {
+        boxes: prevBoard.boxes,
+        activePiece: {
+          ...prevBoard.activePiece!,
+          row: newBoard.activePiece!.row,
+        },
+      };
     });
   };
 
   const softDrop = () => {
-    setActivePiece((prevActivePiece) =>
-      pieceCanFall(prevActivePiece!)
-        ? { ...prevActivePiece!, row: prevActivePiece!.row + 1 }
-        : prevActivePiece
+    setBoard((prevBoard) =>
+      pieceCanFall(prevBoard)
+        ? {
+            boxes: prevBoard.boxes,
+            activePiece: {
+              ...prevBoard.activePiece!,
+              row: prevBoard.activePiece!.row + 1,
+            },
+          }
+        : prevBoard
     );
   };
 
   const shiftLeft = () => {
-    setActivePiece((prevActivePiece) => {
-      for (const [col, row] of getSquaresOfPiece(prevActivePiece!)) {
-        if (col === 0 || boxes[row][col - 1]) {
-          return prevActivePiece;
+    setBoard((prevBoard) => {
+      for (const [col, row] of getSquaresOfPiece(prevBoard.activePiece!)) {
+        if (col === 0 || prevBoard.boxes[row][col - 1] !== "") {
+          return prevBoard;
         }
       }
-      return { ...prevActivePiece!, col: prevActivePiece!.col - 1 };
+      return {
+        boxes: prevBoard.boxes,
+        activePiece: {
+          ...prevBoard.activePiece!,
+          col: prevBoard.activePiece!.col - 1,
+        },
+      };
     });
   };
 
   const shiftRight = () => {
-    setActivePiece((prevActivePiece) => {
-      for (const [col, row] of getSquaresOfPiece(prevActivePiece!)) {
-        if (col === boxesAcross - 1 || boxes[row][col + 1]) {
-          return prevActivePiece;
+    setBoard((prevBoard) => {
+      for (const [col, row] of getSquaresOfPiece(prevBoard.activePiece!)) {
+        if (col === boxesAcross - 1 || prevBoard.boxes[row][col + 1] !== "") {
+          return prevBoard;
         }
       }
-      return { ...prevActivePiece!, col: prevActivePiece!.col + 1 };
+      return {
+        boxes: prevBoard.boxes,
+        activePiece: {
+          ...prevBoard.activePiece!,
+          col: prevBoard.activePiece!.col + 1,
+        },
+      };
     });
   };
 
   const rotateClockwise = () => {
-    setActivePiece((prevActivePiece) => ({
-      ...prevActivePiece!,
-      clockwiseRotation: (prevActivePiece!.clockwiseRotation + 0.25) % 1,
+    setBoard((prevBoard) => ({
+      boxes: prevBoard.boxes,
+      activePiece: {
+        ...prevBoard.activePiece!,
+        clockwiseRotation: (prevBoard.activePiece!.clockwiseRotation + 0.25) % 1,
+      },
     }));
   };
 
   const rotateCounterClockwise = () => {
-    setActivePiece((prevActivePiece) => ({
-      ...activePiece!,
-      clockwiseRotation: (prevActivePiece!.clockwiseRotation + 0.75) % 1,
+    setBoard((prevBoard) => ({
+      boxes: prevBoard.boxes,
+      activePiece: {
+        ...prevBoard.activePiece!,
+        clockwiseRotation: (prevBoard.activePiece!.clockwiseRotation + 0.75) % 1,
+      },
     }));
   };
 
   const periodicFall = () => {
     if (!paused && !ended) {
-      if (activePiece) {
+      if (board.activePiece) {
         softDrop();
       }
       setTimeout(periodicFall, gameSpeed);
     }
   };
 
-  const pieceCanFall = (piece: IActivePiece) => {
-    for (var [col, row] of getSquaresOfPiece(piece)) {
-      if (row === boxesDown - 1 || boxes[row + 1][col]) {
+  const pieceCanFall = (board: { boxes: string[][]; activePiece: IActivePiece | undefined }) => {
+    for (var [col, row] of getSquaresOfPiece(board.activePiece!)) {
+      if (row === boxesDown - 1 || board.boxes[row + 1][col] !== "") {
         return false;
       }
     }
     return true;
   };
 
-  const placePiece = (piece: IActivePiece) => {
-    const newBoxes = boxes;
-    for (var [col, row] of getSquaresOfPiece(piece)) {
-      newBoxes[row][col] = activePiece!.letter;
-    }
-    setBoxes(newBoxes);
+  const placePiece = () => {
+    setBoard((prevBoard) => {
+      const newBoard = prevBoard;
+      for (var [col, row] of getSquaresOfPiece(board.activePiece!)) {
+        newBoard.boxes[row][col] = board.activePiece!.letter;
+      }
+      return newBoard;
+    });
     generateNewActivePiece();
-    checkForCompleteRows(newBoxes);
+    checkForCompleteRows();
   };
 
-  const checkForCompleteRows = (_boxes: string[][]) => {
-    const completeRowIndices = [];
-    for (let rowIndex = 0; rowIndex < boxesDown; rowIndex++) {
-      let rowFull = true;
-      for (const square of _boxes[rowIndex]) {
-        if (!square) {
-          rowFull = false;
-          break;
+  const checkForCompleteRows = () => {
+    setBoard((prevBoard) => {
+      const newBoard = prevBoard;
+      const completeRowIndices = [];
+      for (let rowIndex = 0; rowIndex < boxesDown; rowIndex++) {
+        let rowFull = true;
+        for (const square of prevBoard.boxes[rowIndex]) {
+          if (!square) {
+            rowFull = false;
+            break;
+          }
+        }
+        if (rowFull) {
+          completeRowIndices.push(rowIndex);
         }
       }
-      if (rowFull) {
-        completeRowIndices.push(rowIndex);
+      for (const completeRowIndex of completeRowIndices) {
+        newBoard.boxes = [initialBoxes[0].map((element) => "")]
+          .concat(newBoard.boxes.slice(0, completeRowIndex))
+          .concat(newBoard.boxes.slice(completeRowIndex + 1));
       }
-    }
-    for (const completeRowIndex of completeRowIndices) {
-      _boxes = [initialBoxes[0].map((element) => "")]
-        .concat(_boxes.slice(0, completeRowIndex))
-        .concat(_boxes.slice(completeRowIndex + 1));
-    }
-    setBoxes((prevBoxes) => _boxes);
-    if (completeRowIndices.length > 0) {
-      setScore((prevScore) => prevScore + rowsToPoints.get(completeRowIndices.length)!);
-      updateGameSpeed();
-    }
+      if (completeRowIndices.length > 0) {
+        setScore((prevScore) => prevScore + rowsToPoints.get(completeRowIndices.length)!);
+        updateGameSpeed();
+      }
+      return newBoard;
+    });
   };
 
   const updateGameSpeed = () => {
@@ -291,7 +332,7 @@ export default function Tetris(props: { user: IUser }) {
           <Col xs={12} md={8}>
             <Stage width={(window.innerWidth * 2) / 3} height={window.innerHeight}>
               <Layer>
-                {boxes.map((row, rowIndex) =>
+                {board.boxes.map((row, rowIndex) =>
                   row.map((square, colIndex) => (
                     <Rect
                       key={rowIndex * boxesAcross + colIndex}
@@ -308,15 +349,15 @@ export default function Tetris(props: { user: IUser }) {
                     />
                   ))
                 )}
-                {activePiece &&
-                  getSquaresOfPiece(activePiece).map((indices) => (
+                {board.activePiece &&
+                  getSquaresOfPiece(board.activePiece).map((indices) => (
                     <Rect
                       x={indices[0] * boxWidth + leftMargin}
                       y={indices[1] * boxWidth}
                       height={boxWidth}
                       width={boxWidth}
                       stroke="black"
-                      fill={activePiece!.color}
+                      fill={board.activePiece!.color}
                     />
                   ))}
               </Layer>
